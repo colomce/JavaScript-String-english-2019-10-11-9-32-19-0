3216e877c6f3b8af8898e000860dde6169ac302d
Cedric Don Coloma <colomce@oocl.com>
2020-09-30 17:42:28 +0800

    Capitalize value of name variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
